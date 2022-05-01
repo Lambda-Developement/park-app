7b7e9b62f0b96def887b71af940ac89ba254e4b7
huscker <egostalker03@mail.ru>
2022-05-01 12:56:55 +0300

    Добавил диплинки и редиректы

---
+++ www/js/deeplinks.js
@@ -0,0 +1,52 @@
+// setTimeout(() => {
+document.addEventListener('deviceready', () => {
+
+    // Deeplinks
+    universalLinks.subscribe(null, function (eventData) {
+        console.log(eventData);
+        console.log('Link: ' + eventData.url);
+        console.log('Args: ' + eventData.params);
+
+        if (eventData.url.includes('password')) {
+            // Смена пароля
+            localStorage.conf = eventData.url.split("/")[eventData.url.split("/").length - 1];
+
+            redirect("/screens/ResetPassword/resetPassword.html");
+        }
+        else if (eventData.url.includes('mail')) {
+            // Подтверждение регистрации
+            conf = eventData.url.split("/")[eventData.url.split("/").length - 1];
+            console.log(`api_regconf(${conf})`);
+            api_regconf(conf).then(
+                (success) => {
+                    redirect("/screens/MailConfirmation/mailConfirmation.html");
+                }
+                , (fail) => {
+                    redirect("/screens/MailConfirmation/mailConfirmationFail.html");
+                });
+        }
+    });
+
+    // Forbid changing orientation
+    window.screen.orientation.lock('portrait');
+
+    // Redirect to login if invalid key and skip login if key
+    try {
+        if (isIndex) redirect("/screens/Login/login.html");
+    } catch (except) { }
+    var cur_page = location.href.split('/').slice(-1)[0];
+    const no_redirect = [
+        "login.html",
+        "resetPassword.html",
+        "forgetPassword.html",
+        "registration.html",
+        "resetPassword.html",
+        "mailConfirmation.html",
+        "mailConfirmationInfo.html",
+        "index.html"
+    ];
+    api_kval(localStorage.getItem('auth_key')).then(
+        () => { if (cur_page == "login.html") redirect("../Map/map.html") },
+        () => { if (!no_redirect.includes(cur_page)) redirect_login(); });
+});
+// }, 3000);
---
+++ www/js/redirects.js
@@ -0,0 +1,57 @@
+function redirect(url) {
+    document.location.href = url;
+}
+function redirect_access_error(){
+    redirect('/screens/AccessError/accessError.html');
+}
+function redirect_email(){
+    redirect('/screens/email/emailConfirmation.html');
+}
+function redirect_favourites(){
+    redirect('/screens/Favourites/favourites.html');
+}
+function redirect_forget_password(){
+    redirect('/screens/ForgetPassword/forget_password.html');
+}
+function redirect_home(){
+    redirect('/screens/Home/home.html');
+}
+function redirect_license(){
+    redirect('/screens/License/license.html');
+}
+function redirect_loading(){
+    redirect('/screens/Loading/loading.html');
+}
+function redirect_login(){
+    redirect('/screens/Login/login.html');
+}
+function redirect_mail_confirmation(){
+    redirect('/screens/MailConfirmation/mailConfirmation.html');
+}
+function redirect_map(){
+    redirect('/screens/Map/map.html');
+}
+function redirect_not_available(){
+    redirect('/screens/NotAvailable/not_available.html');
+}
+function redirect_parkzone(){
+    redirect('/screens/Parkzone/park_info.html');
+}
+function redirect_profile_avatar(){
+    redirect('/screens/Profile+avatar/profile_main.html');
+}
+function redirect_profile_edit(){
+    redirect('/screens/ProfileEdit/profile_main.html');
+}
+function redirect_registration(){
+    redirect('/screens/Registration/registration.html');
+}
+function redirect_report_bug(){
+    redirect('/screens/ReportBug/index.html');
+}
+function redirect_reset_password(){
+    redirect('/screens/ResetPassword/resetPassword.html');
+}
+function redirect_reset_password_email(){
+    redirect('/screens/ResetPasswordEmail/resetPassword.html');
+}
